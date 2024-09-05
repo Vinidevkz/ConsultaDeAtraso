8b5f70032d82c96ba8f48b5fb58db6dcdb998714
Vinicius Eduardo <149980701+Vinidevkz@users.noreply.github.com>
2024-09-05 15:54:59 -0300

    banco de dados

---
+++ dbcf.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 05-Set-2024 às 20:54
+-- Versão do servidor: 10.4.28-MariaDB
+-- versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `dbcf`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbatraso`
+--
+
+CREATE TABLE `tbatraso` (
+  `idAtraso` int(11) NOT NULL,
+  `nomeAluno` varchar(60) NOT NULL,
+  `horarioAtraso` varchar(5) NOT NULL,
+  `nomeCurso` varchar(60) NOT NULL,
+  `periodoCurso` varchar(60) NOT NULL,
+  `moduloCurso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tbatraso`
+--
+ALTER TABLE `tbatraso`
+  ADD PRIMARY KEY (`idAtraso`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
